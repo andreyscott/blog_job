76806aa01a25ecdaee04f82c9c7b066e91860e66
andreyscott <andreyscott301@gmail.com>
2024-02-06 21:54:13 +0100

    added the starter files

---
+++ app/page.tsx
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import { Post } from "./lib/interface";
+import { client } from "./lib/sanity";
+
+async function getData() {
+  const query = `*[_type == 'post']`;
+
+  const data = await client.fetch(query);
+
+  return data;
+}
+
+function formatDate(date: string): string {
+  return new Date(date).toISOString().split("T")[0];
+}
+
+export default async function IndexPage() {
+  const data = (await getData()) as Post[];
+
+  return (
+    <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          All Posts
+        </h1>
+      </div>
+
+      <ul>
+        {data.map((post) => (
+          <li className="py-4" key={post._id}>
+            <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+              <div>
+                <p className="text-base font-medium leading-6 text-teal-500">
+                  {formatDate(post._createdAt)}
+                </p>
+              </div>
+
+              <Link
+                href={`/post/${post.slug.current}`}
+                className="space-y-3 xl:col-span-3"
+                prefetch
+              >
+                <div>
+                  <h3 className="text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100">
+                    {post.title}
+                  </h3>
+                </div>
+
+                <p className="prose max-w-none text-gray-500 dark:text-gray-400 line-clamp-2">
+                  {post.overview}
+                </p>
+              </Link>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
